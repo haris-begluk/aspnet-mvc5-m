235166cc2fd8e3000518a8860ac3dfe590ec93d5
HarisBegluk <haris.begluk.webprogramming@outlook.com>
2018-03-11 21:47:42 +0100

    Birthdate added to Customers class and displayed to view

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-        [Required] 
+        [Required]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
